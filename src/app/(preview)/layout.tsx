4df94bc56b7b475aac894f89f10687e5a6c83400
Øystein Remøy <oystein.remoy@gmail.com>
2026-02-21 16:30:19 +0100

    layout

---
+++ src/app/(preview)/layout.tsx
@@ -0,0 +1,15 @@
+// /Users/oystein/nettsider/smootday-v2-feb-26/src/app/(preview)/layout.tsx
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function PreviewLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const cookieStore = await cookies();
+  const unlocked = cookieStore.get("smooday_preview")?.value === "1";
+
+  if (!unlocked) redirect("/unlock");
+  return <>{children}</>;
+}
